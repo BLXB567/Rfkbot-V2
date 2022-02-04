b3e86fb9835603fef5b21456322759b0a5e79923
BLXB567 <97624291+BLXB567@users.noreply.github.com>
2022-02-04 11:20:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,11 +14,11 @@
 gc1 = ''
 gc2 = ''
 gc3 = ''
-global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
-global.owner = ['+1 (775) 403-5736','6289505165400'] // Put your number here //owner eval
-global.kontak = ['+1 (775) 403-5736','6289505165400'] //Ketika ada yang ngetik #owner
+global.linkGC = ['https://chat.whatsapp.com/Ie3xBHdL22A560EBWRwFwI', 'https://chat.whatsapp.com/GsZcFsjIuVOGlJYIgrTJzg'] // ganti jadi group lu
+global.owner = ['+1 (859) 710-9548','6283117436733'] // Put your number here //owner eval
+global.kontak = ['+1 (859) 710-9548','6283117436733'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['+1 (775) 403-5736','6289505165400','6282287750102'] // Premium user has unlimited limit
+global.prems = ['6281331493321','6285708561219','6283117436733'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -49,20 +49,20 @@ xteamkey = 'Dawnfrostkey'
 namaig = 'GAK ADA :)'
 namagithub = 'GAK ADA :)'
 kasihcaption = `Nih kak`
-namakontak1 = 'AlyaaXzy 1'
-namakontak2 = 'AlyaaXzy 2'
+namakontak1 = 'Syah 1'
+namakontak2 = 'Miyuki 2'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'By AlyaaXzy' // ganti aja
-global.author = 'AlyaaXzy' // ganti aja
+global.packname = 'Yae sticker' // ganti aja
+global.author = 'BLZXB5' // ganti aja
 
 //yyy
 bc = 'AlyaaXzy' //AlyaaXy Broadcast
-footer = '\n©AlyaaXzy'
-namabot = 'AlyaaXzy'
-namalu = 'AlyaaXzy'
+footer = '\n©Yae DVT'
+namabot = 'Yae DVT'
+namalu = 'BLZXB5'
 
 
 // 
